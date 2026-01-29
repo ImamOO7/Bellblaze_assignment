d1263c948723af9418ea6461fccf52960165cfdd
Md Naiyar Imam <naiyarimam14@gmail.com>
2026-01-29 23:39:29 +0530

    assigment

---
+++ assign1.py
@@ -0,0 +1,19 @@
+list = [14,19,2,1,18]
+#let suppose i have to insert 25 at index of 2(first question)
+list.insert(2, 25)
+print(list)#2nd question
+# 3rd question is to remove first element from the list
+list.pop(0)
+print(list)
+# 4rth question is to insert element at last of the list
+list.append(45)
+print(list)
+# 5th question is to sort the list
+list.sort()
+print(list)
+# 6th question is pop the last element from the list
+list.pop()
+print(list)
+# 7th question is to reverse the list
+list.reverse()
+print(list)
---
+++ assign2.py
@@ -0,0 +1,30 @@
+list=[] # first of all create an empty list and then add soome values to it
+list.append(14)
+list.append(19)
+list.append(25)
+list.append(118)
+print(list) # first task done
+
+# let suppose i have to insert a value at 1 index 
+list.insert(1,45)
+print(list) # second task done
+
+
+
+# now perform all methods which is listed in assingment 1
+# delete the first element from the list
+list.pop(0)
+print(list) 
+
+# for sorting the list use sort() method
+list.sort()
+print(list)
+
+#to remove the last element from the list 
+list.pop()
+print(list)
+
+
+# to reverse the list use reverse() method
+list.reverse() 
+print(list)
